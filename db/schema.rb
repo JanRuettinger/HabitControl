d4c0eb786e1435ea6e6dd9d679e87e2970354788
JanRuettinger <jan-hendrik.ruettinger@online.de>
2016-01-19 13:45:52 +0700

    Setup basic gym table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119045017) do
+ActiveRecord::Schema.define(version: 20160119064123) do
+
+  create_table "gyms", force: :cascade do |t|
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "done",       default: false
+    t.boolean  "flag",       default: false
+  end
 
   create_table "sleeps", force: :cascade do |t|
     t.datetime "created_at",                  null: false
